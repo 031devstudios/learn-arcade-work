e37d503d1e080688b3fa1f85f9bc141c06ac452c
Josh Robb <031devstudios@gmail.com>
2023-11-08 20:27:34 +0200

    Made lots of changes

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,8 @@
+print("This is me learning to use the arcade library in Python.")
+print("I have no previous coding experience.")
+print("""This is a
+      multiple line
+      print""")
+print("\n")
+
+print("I've learn that I am able to use a \\ symbol in code by placing a backslash before the character eg. \\\ ")
---
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -0,0 +1,27 @@
+"""
+This is a sample program to show how to draw using the Python programming
+language and the Arcade library.
+
+Multi-line comments are surrounded by three double-quote marks.
+Single-line comments start with a hash/pound sign. #
+"""
+
+# Import the "arcade" library
+
+import arcade
+
+arcade.open_window(600, 600, "Drawing Example")
+
+# Set background colour
+arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+
+# Get ready to draw
+arcade.start_render()
+
+# Drwaing goes here
+
+# Finish drwaing
+arcade.finish_render()
+
+# Keep the window open
+arcade.run()
